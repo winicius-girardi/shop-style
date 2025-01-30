1551b6e2aab0a27ab3a595fc73cc191e995d1c80
Winicius Girardi <wgirardi541@gmail.com>
2025-01-30 05:59:17 -0300

    feat: initial ms-catalog

---
+++ ms-catalog/src/test/kotlin/br/com/ms_catalog/MsCatalogApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.ms_catalog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsCatalogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
